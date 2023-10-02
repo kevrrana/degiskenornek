933ef748f01f4dbde4bf5b9225a9959efb1b606d
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-02 13:35:06 +0300

    Add files via upload

---
+++ degisken-ornek1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace degisken_ornek1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblToplama = new System.Windows.Forms.Label();
+            this.lblCikarma = new System.Windows.Forms.Label();
+            this.lblCarpma = new System.Windows.Forms.Label();
+            this.lblBolme = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(39, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Sayı";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(115, 35);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(115, 61);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(82, 105);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(88, 37);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblToplama
+            // 
+            this.lblToplama.AutoSize = true;
+            this.lblToplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplama.Location = new System.Drawing.Point(244, 38);
+            this.lblToplama.Name = "lblToplama";
+            this.lblToplama.Size = new System.Drawing.Size(75, 15);
+            this.lblToplama.TabIndex = 5;
+            this.lblToplama.Text = "Toplama : ";
+            // 
+            // lblCikarma
+            // 
+            this.lblCikarma.AutoSize = true;
+            this.lblCikarma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCikarma.Location = new System.Drawing.Point(249, 61);
+            this.lblCikarma.Name = "lblCikarma";
+            this.lblCikarma.Size = new System.Drawing.Size(72, 15);
+            this.lblCikarma.TabIndex = 6;
+            this.lblCikarma.Text = "Çıkarma : ";
+            // 
+            // lblCarpma
+            // 
+            this.lblCarpma.AutoSize = true;
+            this.lblCarpma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCarpma.Location = new System.Drawing.Point(249, 83);
+            this.lblCarpma.Name = "lblCarpma";
+            this.lblCarpma.Size = new System.Drawing.Size(69, 15);
+            this.lblCarpma.TabIndex = 7;
+            this.lblCarpma.Text = "Çarpma : ";
+            // 
+            // lblBolme
+            // 
+            this.lblBolme.AutoSize = true;
+            this.lblBolme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBolme.Location = new System.Drawing.Point(256, 105);
+            this.lblBolme.Name = "lblBolme";
+            this.lblBolme.Size = new System.Drawing.Size(60, 15);
+            this.lblBolme.TabIndex = 8;
+            this.lblBolme.Text = "Bölme : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 252);
+            this.Controls.Add(this.lblBolme);
+            this.Controls.Add(this.lblCarpma);
+            this.Controls.Add(this.lblCikarma);
+            this.Controls.Add(this.lblToplama);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblToplama;
+        private System.Windows.Forms.Label lblCikarma;
+        private System.Windows.Forms.Label lblCarpma;
+        private System.Windows.Forms.Label lblBolme;
+    }
+}
+
---
+++ degisken-ornek1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace degisken_ornek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, toplama,carpma, cikarma, bolme;
+
+            sayi1 = Convert.ToDouble(txtSayi1.Text);
+            sayi2 = Convert.ToDouble(txtSayi2.Text);
+
+            toplama = sayi2 + sayi1;
+            cikarma = sayi2 - sayi1;
+            carpma = sayi2 * sayi1;
+            bolme = sayi2 / sayi1;
+
+            lblToplama.Text = "Toplama: " + toplama;
+            lblCikarma.Text = "Çıkarma: " + cikarma;
+            lblCarpma.Text = "Çarpma: " + carpma;
+            lblBolme.Text = "Bölme: " + bolme;
+
+
+
+        }
+    }
+}
